a7b96eec63e5286cfadef5bac92fb98372a3a80e
songbaone <baob2014966@student.ctu.edu.vn>
2026-06-28 18:55:46 +0000

    Update new code

--- server/src/modules/payments/routes.js
+++ server/src/modules/payments/routes.js
@@ -1,22 +1,13 @@
 const express = require('express');
 const authenticate = require('../../middleware/authenticate');
-const authorize = require('../../middleware/authorize');
 const validate = require('../../middleware/validate');
-const { ROLES } = require('../../config/constants');
 const controller = require('./controller');
-const {
-  paymentIdParamValidation,
-  listPaymentsQueryValidation,
-  updatePaymentStatusValidation,
-} = require('./validation');
+const { confirmPaymentValidation } = require('./validation');
 
 const router = express.Router();
 
 router.use(authenticate);
-router.use(authorize(ROLES.ADMIN, ROLES.STAFF));
 
-router.get('/', listPaymentsQueryValidation, validate, controller.getPayments);
-router.get('/:id', paymentIdParamValidation, validate, controller.getPaymentById);
-router.patch('/:id/status', updatePaymentStatusValidation, validate, controller.updatePaymentStatus);
+router.post('/vnpay/confirm', confirmPaymentValidation, validate, controller.confirmPayment);
 
 module.exports = router;
